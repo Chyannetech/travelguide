601ef85857b5349971ac0fa4917ed81ba2a08378
Chyanne Robbins <103911002+Chyannetech@users.noreply.github.com>
2024-02-17 19:27:04 -0500

    Add logic to search bar

--- src/components/SearchFilter.js
+++ src/components/SearchFilter.js
@@ -1,15 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 function SearchFilter({ placeholder, data }) {
+  // Create a state to hold filtered data list 
+  const [filteredData, setFilteredData] = useState([]);
+// Pass the event of the input to access values
+  const handleFilter = (event) => {
+    const searchData = event.target.value;
+    // Only show data that is searched for 
+    const newFilter = data.filter((value) => {
+      return value.name.toLowerCase().includes(searchData.toLowerCase());
+    });
+    // Hide data until a search matches proper filter values
+    if (searchData === "") {
+      setFilteredData([]);
+    } else {
+      setFilteredData(newFilter);
+    }
+  };
+  // Add search bar with placehoder and event handler
   return (
-    <div className="search">
-      <div className="input"></div>
-      <input type="text" placeholder={placeholder}></input>
-      <div className="filteredData"></div>
-      {/* Loops through data to return names of places*/}
-      {data.map((value, key) => {
-        return <div>{value.name}</div>;
-      })}
+    <div className="searchBar">
+      <div className="input">
+        <input
+          type="text"
+          placeholder={placeholder}
+          onChange={handleFilter}
+        ></input>
+      </div>
+      
+      {/* Only show filtered data when array is not empty */}
+      {filteredData.length != 0 && (
+        <div className="filteredData">
+          {filteredData.map((value, key) => {
+            return (
+              <div>
+                <a className="dataName" href={value.link} target="_blank">
+                  <p>{value.name}</p>
+                </a>
+                <p>{value.city}</p>
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
